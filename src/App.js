0fa6d952cc53456f87e8500a8c4e22858aa506b5
Maryam482 <jamilmaryam593@gmail.com>
2021-02-16 15:55:32 +0500

    useeffect and fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React , { useState, useEffect } from 'react';
+import axios from 'axios';
+import react from 'react';
 
 
 
 function App() {
 
-const [value,setValue ] = useState();
+const [posts,setPosts ] = useState([]);
+const [click,setClick] = useState()
 
 
-function change () {
-        setValue (value+1)
-}
+// function change () {
+//         setValue (value+1)
+     
+// }
 
 useEffect(() => {
   alert("Iam working ")
-})
+  // setValue (value +1)
+
+  axios.get(`https://www.reddit.com/r/reactjs.json`)
+  .then (res => {
+    const newPosts = res.data.data.children
+    .map(obj => obj.data)
+    setPosts(newPosts)
+  });
+},[click]);
   return (
     <div>
-      
-<button onClick = {change} >Click Me</button>
+   <h1>/r/reactjs</h1> 
+
+<button onClick={ ()=>setClick(!click)  }>Reload</button>
+<ul>
+  {posts.map(post => (
+    <li key={post.id}>{post.title}</li>
+
+    
+  ))}
+</ul>
 
     </div>
   );
